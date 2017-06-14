32d3af110752e51d56c939e50f402ccb5b9cf9ac
Surya Singh <surya2902@gmail.com>
2017-06-14 22:08:26 +0530

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    function signIn() {
+      
+    }
+    function signUp() {
+
+    }
+});
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var db = require('../config/db');
+
+router.get('/', function(req, res) {
+  db.getConnection(function(err, connection){
+         if (!err) {
+           connection.query('SELECT * from user' , function(err, rows) {
+             res.render('pages/index',{
+               data: JSON.stringify(rows)
+             })
+           })
+         } else {
+           console.log('Error while performing Query.');
+           console.log(err);
+
+         }
+  });
+});
+
+module.exports = router;
